8a404ff0f6c798605eb55c3181712f7b520df4ec
mehejabinjannat <mehejabinjannat4@gmail.com>
2026-06-15 11:50:02 +0600

    Update Shape.java

--- shapes/Shape.java
+++ shapes/Shape.java
@@ -1,16 +1,14 @@
 package shapes;
 
 public abstract class Shape {
-    private String color;
+
+    private String  color;
     private boolean filled;
 
     public Shape(String color, boolean filled) {
-        this.color = color;
+        this.color  = color;
         this.filled = filled;
     }
-
-    public String getColor() { return color; }
-    public boolean isFilled() { return filled; }
 
     public abstract double getArea();
     public abstract double getPerimeter();
@@ -20,4 +18,9 @@ public abstract class Shape {
         return "Color : " + color + "\n" +
                "Filled: " + (filled ? "Yes" : "No");
     }
+
+    public String  getColor()           { return color;  }
+    public boolean isFilled()           { return filled; }
+    public void    setColor(String c)   { this.color  = c; }
+    public void    setFilled(boolean f) { this.filled = f; }
 }
