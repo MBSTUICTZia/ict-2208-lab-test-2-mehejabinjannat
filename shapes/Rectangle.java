3eb66c716469b99225db36a8f6d01278eb71789c
mehejabinjannat <mehejabinjannat4@gmail.com>
2026-06-15 11:41:52 +0600

    Update Rectangle.java

--- shapes/Rectangle.java
+++ shapes/Rectangle.java
@@ -25,11 +25,10 @@ public class Rectangle extends Shape {
 
     @Override
 public String toString() {
-    return "[ shapes.Rectangle ]\n" +
+    return "[ shapes.Circle ]\n" +
            super.toString() + "\n" +
-           "Width : " + width + "\n" +
-           "Length: " + length + "\n" +
-           String.format("Area      : %.2f", getArea()) + "\n" +
-           String.format("Perimeter : %.2f", getPerimeter());
+           "Radius: " + radius + "\n" +
+           String.format("Area         : %.2f", getArea()) + "\n" +
+           String.format("Circumference: %.2f", getPerimeter());
 }
 }
